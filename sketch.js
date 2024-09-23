12be06a9a1df365066f30d9d8643b42b0c3fb3a0
pararaios <arthur.sobral@escola.pr.gov.br>
2024-09-23 15:32:17 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+function setup() {
+  createCanvas(400 , 400);
+}
+
+function draw() {
+  background("red");
+ fill("white")
+  ellipse(200, 200, 110, 110 );
+  stroke("white")
+  ellipse(200 , 300, 170, 170);
+  stroke("white")
+  ellipse(200, 110, 90 ,90);
+  stroke("white");
+  
+  fill("black")
+  ellipse(185, 99, 20, 20);
+ fill("black")
+  ellipse(215, 99, 20, 20);
+  
+  fill("black")
+  ellipse(200,180,10,10)
+  ellipse(200,200,10,10)
+  ellipse(200,220,10,10)
+  
+  
+  fill("orange")
+  triangle(200,130,145,133,200,115)
+  
+  stroke("brown")
+  line(249,196,318,170)
+  line(152,196,65,168)
+  line(318,152,280,185)
+  line(318,189,280,185)
+  line(67,190,104,180)
+  line(67,148,104,180)
+}
